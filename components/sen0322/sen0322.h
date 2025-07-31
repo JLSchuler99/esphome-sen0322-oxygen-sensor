5e864ae6f0c78d83beb12ba5d989733a9f7ad3aa
Jacob Schuler <j@cob.sh>
2025-07-30 20:24:00 -0400

    Update sen0322.h
    add ema_oxygen_

--- components/sen0322/sen0322.h
+++ components/sen0322/sen0322.h
@@ -15,6 +15,7 @@ class SEN0322Sensor : public PollingComponent, public i2c::I2CDevice, public sen
   float get_setup_priority() const override;
 
  protected:
+   float ema_oxygen_{NAN};
   // Additional methods can be added here if needed for advanced functionality
 };
 
